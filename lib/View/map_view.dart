1a94167e0d4a337824479f30c8d0e10cdc5aa5e3
YHW505 <guddn505@naver.com>
2025-09-06 15:06:50 +0900

    지도 추가

--- lib/View/map_view.dart
+++ lib/View/map_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import '../Controller/map_controller.dart';
@@ -9,6 +10,53 @@ class MapView extends GetView<MapController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("map view")));
+    // 화면이 빌드될 때 한 번만 실행
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!controller.isLocationLoaded.value) {
+        controller.initializeLocation();
+      }
+    });
+    return Obx(
+      () => SafeArea(
+        child: Stack(
+          children: [
+            NaverMap(
+              options: NaverMapViewOptions(
+                initialCameraPosition: NCameraPosition(
+                  target: controller.userPosition.value != null
+                      ? NLatLng(controller.userPosition.value!.latitude, controller.userPosition.value!.longitude)
+                      : const NLatLng(37.5665, 126.9780),
+                  zoom: 15,
+                ),
+              ),
+              onMapReady: (mController) async {
+                if (controller.isMapReady.value == false) {
+                  // await controller.fetchMyChargers(context, null);
+                  controller.nMapController = mController;
+                  controller.isMapReady.value = true;
+                  // controller.loadMarkers(controller.chargers); // 서버에서 충전소 받아와서 마커 표시
+                }
+              },
+            ),
+            // Positioned(
+            //   top: -20,
+            //   child: SearchAppbarWidget(
+            //     onTap: () async {
+            //       final result = await Navigator.push(context, MaterialPageRoute(builder: (_) => SearchChargerView()));
+            //       await fetchMyChargers(context, result);
+            //     },
+            //   ),
+            // ),
+            controller.isMapReady.value
+                ? Positioned(
+                    bottom: 0,
+                    child: SizedBox.shrink(),
+                    // child: SlidingupPanelWidget(chargers: _chargers, nMapController: _nMapController, boxController: _boxController),
+                  )
+                : SizedBox.shrink(),
+          ],
+        ),
+      ),
+    );
   }
 }
