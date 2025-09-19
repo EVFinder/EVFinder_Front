2f36db269d15997b2cbc0f12c39bc551b20842b4
YHW505 <guddn505@naver.com>
2025-09-19 18:53:35 +0900

    bottomNavigationBar 복구(2)

--- lib/Controller/main_controller.dart
+++ lib/Controller/main_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 
+import 'package:evfinder_front/View/profile_view.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import '../View/map_view.dart';
@@ -7,7 +8,7 @@ import '../View/favortie_station_view.dart';
 
 class MainController extends GetxController {
   RxInt selectedIndex = 1.obs;
-  List<Widget> pages = <Widget>[FavoriteStationView(), MapView()];
+  List<Widget> pages = <Widget>[FavoriteStationView(), MapView(), ProfileView()];
 
 
   void setView(int index) {
