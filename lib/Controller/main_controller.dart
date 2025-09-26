e89eeb8dae12a9ed4523cab8d6cbbbf02d239134
YHW505 <guddn505@naver.com>
2025-09-26 20:53:11 +0900

    커뮤니티 파일구조 생성

--- lib/Controller/main_controller.dart
+++ lib/Controller/main_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 
+import 'package:evfinder_front/View/community_view.dart';
 import 'package:evfinder_front/View/profile_view.dart';
 import 'package:evfinder_front/View/host_view.dart';
 import 'package:get/get.dart';
@@ -8,8 +9,8 @@ import '../View/map_view.dart';
 import '../View/favortie_station_view.dart';
 
 class MainController extends GetxController {
-  RxInt selectedIndex = 1.obs;
-  List<Widget> pages = <Widget>[FavoriteStationView(), MapView(), HostView(), ProfileView()];
+  RxInt selectedIndex = 2.obs;
+  List<Widget> pages = <Widget>[CommunityView(),FavoriteStationView(), MapView(), HostView(), ProfileView()];
 
 
   void setView(int index) {
