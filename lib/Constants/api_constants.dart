024b2627e54d54d96593027a31de66455ad1c485
YHW505 <guddn505@naver.com>
2025-09-26 21:37:37 +0900

    날씨 -진짜 완- (주소 수정 완료)

--- lib/Constants/api_constants.dart
+++ lib/Constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   //본인 서버 주소 사용
-  static const String ip = '100.100.102.243'; // IP 만 바꿔주면 됨
+  static const String ip = '100.100.108.96'; // IP 만 바꿔주면 됨
   static const String baseUrl = 'http://$ip:8080';
 
   static const String authApiBaseUrl = 'http://$ip:8080/auth';
@@ -11,6 +11,7 @@ class ApiConstants {
   static const String chargerbnbApiUrl = '$baseUrl/share';
   static const String reviewBaseUrl = '$baseUrl/api/review';
   static const String reservApiBaseUrl = '$baseUrl/reserve';
+  static const String coorToAddr = '$baseUrl/place/address';
 
 
 
