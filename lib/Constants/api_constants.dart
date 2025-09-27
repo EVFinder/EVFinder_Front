c0ad1a554fb9d4da933b8fbe5df38fbd3d7f2e04
YHW505 <guddn505@naver.com>
2025-09-27 17:54:09 +0900

    커뮤니티 중간 커밋 - 수정중(3)

--- lib/Constants/api_constants.dart
+++ lib/Constants/api_constants.dart
@@ -1,9 +1,10 @@
 class ApiConstants {
   //본인 서버 주소 사용
-  static const String ip = '100.100.108.96'; // IP 만 바꿔주면 됨
+  static const String ip = '52.78.153.27'; // IP 만 바꿔주면 됨
+  // static const String ip = '100.100.108.96'; // IP 만 바꿔주면 됨
   static const String baseUrl = 'http://$ip:8080';
 
-  static const String authApiBaseUrl = 'http://$ip:8080/auth';
+  static const String authApiBaseUrl = '$baseUrl/auth';
   static const String evApiBaseUrl = '$baseUrl/api/charger/';
   static const String favoriteApiBaseUrl = '$baseUrl/api/favorite';
   static const String weatherApiBaseUrl = '$baseUrl/weather';
