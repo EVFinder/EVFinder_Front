b6a86a8d00f562f58b6a3b3e24e239f8240586c3
eunji56 <mefdll2g35@gmail.com>
2025-09-29 15:04:20 +0900

    충전소 등록 수정

--- lib/Controller/register_charge_controller.dart
+++ lib/Controller/register_charge_controller.dart
@@ -40,8 +40,23 @@ class RegisterChargeController extends GetxController {
     userName = prefs.getString('name');
   }
 
+  void resetForm() {
+    addrController.clear();
+    detailaddrController.clear();
+    phoneController.clear();
+    chargeNameContrller.clear();
+    chargeTypeController.clear();
+    statController.clear();
+    powerController.clear();
+    priceContoller.clear();
+
+    selectedStat.value = null;
+    lat.value = null;
+    lon.value = null;
+  }
+
   @override
-  void dispose() {
+  void onClose() {
     addrController.dispose();
     detailaddrController.dispose();
     phoneController.dispose();
@@ -50,7 +65,9 @@ class RegisterChargeController extends GetxController {
     statController.dispose();
     powerController.dispose();
     priceContoller.dispose();
+    super.onClose();
   }
+
   Future<void> openPostcode() async {
     await Get.to(() => KpostalView(
       callback: (Kpostal result) {
@@ -64,7 +81,7 @@ class RegisterChargeController extends GetxController {
     ));
   }
 
-  Future <void> register(BuildContext context) async {
+  Future <bool> register(BuildContext context) async {
     final address = '${addrController.text.trim()} ${detailaddrController.text.trim()}'.trim();
     final hostContact = phoneController.text;
     final stationName = chargeNameContrller.text;
@@ -76,7 +93,7 @@ class RegisterChargeController extends GetxController {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('모든 필드를 채워주세요.')),
       );
-      return;
+      return false;
     }
 
     try {
@@ -99,15 +116,19 @@ class RegisterChargeController extends GetxController {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('등록 완료!')),
         );
+        resetForm();
+        return true;
       }else {
         // 서버가 에러 메시지를 준다면 보여주기
         final msg = response.body.isNotEmpty ? response.body : '요청 실패';
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('등록 실패(${response.statusCode}) : $msg')),
         );
+        return false;
       }
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('등록 실패: ${e.toString()}')));
+      return false;
     }
   }
 }
