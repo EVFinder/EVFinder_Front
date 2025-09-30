b69ef1c8ddc08d2ab601de292b5c85f82a67e0f6
eunji56 <mefdll2g35@gmail.com>
2025-09-30 18:08:58 +0900

    예약

--- lib/Controller/charge_detail_controller.dart
+++ lib/Controller/charge_detail_controller.dart
@@ -21,7 +21,7 @@ class ChargeDetailController extends GetxController {
   void onInit() {
     super.onInit();
     _loadUid();
-    loadReview();
+
     final arguments = Get.arguments as Map<String, dynamic>?;
     if(arguments != null) {
       final stationData = arguments['station'] as Map<String, dynamic>?;
@@ -31,7 +31,9 @@ class ChargeDetailController extends GetxController {
         print("리뷰 페이지에 받아 온 정보 : $stationId");
       }
     }
-    loadReview();
+    if (stationId?.isNotEmpty == true) {
+      loadReview();
+    }
   }
 
   Future<void> _loadUid() async {
