b505a2a98c92d1a67f624a765fc2c1445abf4b58
eunji56 <mefdll2g35@gmail.com>
2025-10-10 10:57:06 +0900

    bnb 위치 수정, 호스트 예약 카드 변경

--- lib/View/reserv_management_view.dart
+++ lib/View/reserv_management_view.dart
@@ -11,8 +11,10 @@ class ReservManagementView extends GetView<ReservManagementController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFF7F9FC),
       appBar: AppBar(
         title: const Text("예약 관리"),
+        backgroundColor: Colors.white,
       ),
       body: Obx(() {
         if (controller.isLoading.value) {
@@ -33,20 +35,55 @@ class ReservManagementView extends GetView<ReservManagementController> {
             final startTimeString = reservation['startTime'];
             final endTimeString = reservation['endTime'];
 
-            if (startTimeString != null && endTimeString != null) {
-              try {
-                final startTime = DateTime.parse(startTimeString);
-                final endTime = DateTime.parse(endTimeString);
+            DateTime? startTime;
+            DateTime? endTime;
 
-                dateText = DateFormat('MM월 dd일').format(startTime);
-                timeText = '${DateFormat('HH:mm').format(startTime)} - ${DateFormat('HH:mm').format(endTime)}';
-              } catch (e) {
-                print("날짜 파싱 에러: $e");
+            if(startTimeString != null) {
+              startTime = DateTime.parse(startTimeString).toLocal();
+            }
+            if(endTimeString != null) {
+              endTime = DateTime.parse(endTimeString).toLocal();
+            }
+            if(startTime != null && endTime != null) {
+              final startdateText = DateFormat('yyyy-MM-dd').format(startTime);
+              final enddateText = DateFormat('yyyy-MM-dd').format(endTime);
+
+              dateText = (startdateText == enddateText) ? startdateText : '$startdateText-$enddateText';
+
+              final startTimePart = DateFormat('HH시 mm분').format(startTime);
+              final endTimePart = DateFormat('HH시 mm분').format(endTime);
+
+              timeText = '$startTimePart-$endTimePart';
+            }
+            final now = DateTime.now().toLocal();
+            bool isExpired = false;
+
+            if(startTime != null) {
+              final start = DateTime(startTime.year, startTime.month, startTime.day);
+              final nowDate = DateTime(now.year, now.month, now.day);
+              if(start.isBefore(nowDate)){
+                isExpired = true;
               }
             }
+            if(endTime != null && endTime.isBefore(now)) {
+              isExpired = true;
+            }
+
+            final statusText = isExpired ? '만료된 예약입니다.' : '예약 확정';
+            // if (startTimeString != null && endTimeString != null) {
+            //   try {
+            //     final startTime = DateTime.parse(startTimeString);
+            //     final endTime = DateTime.parse(endTimeString);
+            //
+            //     dateText = DateFormat('MM월 dd일').format(startTime);
+            //     timeText = '${DateFormat('HH:mm').format(startTime)} - ${DateFormat('HH:mm').format(endTime)}';
+            //   } catch (e) {
+            //     print("날짜 파싱 에러: $e");
+            //   }
+            // }
 
             return ReservHostCard(
-              statusText: '예약 확정',
+              statusText: statusText,
               userName: reservation['userName'] ?? '정보 없음',
               userPhone: reservation['userPNumber'] ?? '정보 없음',
               dateText: dateText,
